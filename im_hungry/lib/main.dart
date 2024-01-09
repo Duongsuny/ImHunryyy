acbd85773c9842b89458c5c22d54ba025e57d037
Duongsuny <duongsunny03@gmail.com>
2024-01-10 00:28:04 +0700

    initial commit

---
+++ im_hungry/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ImHungry());
+}
+
+class ImHungry extends StatelessWidget {
+  const ImHungry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
