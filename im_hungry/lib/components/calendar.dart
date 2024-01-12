7e3403351bae01a26427f88e56cef891774b5ac8
Duongsuny <duongsunny03@gmail.com>
2024-01-12 16:20:42 +0700

    minor changes in calendar

--- im_hungry/lib/components/calendar.dart
+++ im_hungry/lib/components/calendar.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter/material.dart';
 import 'package:im_hungry/colors.dart';
 import "package:table_calendar/table_calendar.dart";
-import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -12,7 +11,14 @@ class Calendar extends StatefulWidget {
 
 class _CalendarState extends State<Calendar> {
   CalendarFormat calendarFormat = CalendarFormat.month;
-  //commong style constants
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<CalendarFormat, String> availableFormats = {
+    CalendarFormat.month: 'Tháng',
+    CalendarFormat.twoWeeks: '2 tuần',
+    CalendarFormat.week: 'Tuần'
+  };
+  //common style constants
   TextStyle bold = TextStyle(
       color: HungryColors().surfaceBrown,
       fontSize: 17,
@@ -25,77 +31,95 @@ class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
     const offset = Offset(2, 2);
-    return SafeArea(
-      child: TableCalendar(
-        calendarFormat: calendarFormat,
-        locale: "vi_VN",
-        onFormatChanged: (format) {
+    return TableCalendar(
+      calendarFormat: calendarFormat,
+      locale: "vi_VN",
+      onFormatChanged: (format) {
+        setState(() {
+          calendarFormat = format;
+        });
+      },
+      firstDay: DateTime.utc(2024, 1, 1),
+      lastDay: DateTime.utc(2030, 3, 14),
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      focusedDay: _focusedDay,
+      availableCalendarFormats: availableFormats,
+      daysOfWeekHeight: 25,
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        if(!isSameDay(_selectedDay, selectedDay)) {
           setState(() {
-            calendarFormat = format;
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
           });
-        },
-        firstDay: DateTime.utc(2024, 1, 1),
-        lastDay: DateTime.utc(2030, 3, 14),
-        startingDayOfWeek: StartingDayOfWeek.monday,
-        focusedDay: DateTime.now(),
-        availableCalendarFormats: const {CalendarFormat.month : 'Tháng', CalendarFormat.twoWeeks : '2 tuần', CalendarFormat.week : 'Tuần'},
-        daysOfWeekHeight: 25,
-        daysOfWeekStyle:
-            DaysOfWeekStyle(weekdayStyle: bold, weekendStyle: bold),
-        headerStyle: HeaderStyle(
-            //headerPadding: const EdgeInsets.only(bottom: 20),
-            formatButtonTextStyle: bold,
-            titleTextStyle: bold,
-            formatButtonDecoration: BoxDecoration(
-                color: HungryColors().backYellow,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 10,
-                    offset: -offset,
-                    color: Colors.white.withOpacity(.2),
-                  ),
-                  BoxShadow(
-                    blurRadius: 10,
-                    offset: offset,
-                    color: Colors.brown.withOpacity(.2),
-                  )
-                ])),
-        calendarStyle: CalendarStyle(
-            tablePadding: EdgeInsets.only(top: 10),
-            weekendTextStyle: normal,
-            defaultTextStyle: normal,
-            todayTextStyle: bold,
-            selectedDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 5,
-                    offset: -offset,
-                    color: Colors.white.withOpacity(.2),
-                  ),
-                  BoxShadow(
-                      blurRadius: 10,
-                      offset: offset,
-                      color: Colors.brown.withOpacity(.3),
-                      inset: true)
-                ]),
-            todayDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: HungryColors().backYellow,
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 5,
-                    offset: -offset,
-                    color: Colors.brown.withOpacity(.1),
-                  ),
-                  BoxShadow(
-                    blurRadius: 5,
-                    offset: offset,
-                    color: Colors.brown.withOpacity(.3),
-                  )
-                ])),
-      ),
+        }
+      },
+      daysOfWeekStyle: DaysOfWeekStyle(weekdayStyle: bold, weekendStyle: bold),
+      headerStyle: HeaderStyle(
+          //headerPadding: const EdgeInsets.only(bottom: 20),
+          formatButtonTextStyle: bold,
+          titleTextStyle: bold,
+          formatButtonDecoration: BoxDecoration(
+              color: HungryColors().backYellow,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 10,
+                  offset: -offset,
+                  color: Colors.white.withOpacity(.2),
+                ),
+                BoxShadow(
+                  blurRadius: 10,
+                  offset: offset,
+                  color: Colors.brown.withOpacity(.2),
+                )
+              ])),
+      calendarStyle: CalendarStyle(
+          tablePadding: const EdgeInsets.only(top: 10),
+          outsideDaysVisible: false,
+          weekendTextStyle: normal,
+          defaultTextStyle: normal,
+          todayTextStyle: bold,
+          selectedTextStyle: normal,
+          selectedDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: HungryColors().backYellow,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 5,
+                  offset: -offset,
+                  color: Colors.brown.withOpacity(.1),
+                ),
+                BoxShadow(
+                  blurRadius: 5,
+                  offset: offset,
+                  color: Colors.brown.withOpacity(.3),
+                ),
+              ]
+          ),
+          defaultDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          weekendDecoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          todayDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: HungryColors().backYellow,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 5,
+                  offset: -offset,
+                  color: Colors.brown.withOpacity(.1),
+                ),
+                BoxShadow(
+                  blurRadius: 5,
+                  offset: offset,
+                  color: Colors.brown.withOpacity(.3),
+                )
+              ])),
     );
   }
 }
--- im_hungry/lib/pages/components_test.dart
+++ im_hungry/lib/pages/components_test.dart
@@ -13,6 +13,29 @@ class ComponentsTest extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         bottomNavigationBar: BottomNavBar(),
-        body: Center(child: Calendar()));
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          offset: const Offset(7, 7),
+                          color: Colors.brown.withOpacity(.3),
+                          inset: true),
+                      BoxShadow(
+                          blurRadius: 10,
+                          offset: const Offset(-7, -7),
+                          color: Colors.brown.withOpacity(.2),
+                          inset: true)
+                    ]),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Calendar(),
+                )),
+          )
+        ]));
   }
 }
